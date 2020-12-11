2fadbb7bfa6455a9c0cabe4fffa1ebfa0da07cc8
monette5 <darcy4@hotmail.com>
2020-12-11 13:28:10 +0000

    Summernote is embedded in the page - nobody breathe on it

--- assets/static/summernoteCall.js
+++ assets/static/summernoteCall.js
@@ -1,6 +1,12 @@
+function addElement (el) { 
 const elem = document.querySelector('textarea.form-control');
-//var newdiv1 = $("<div id='summernote'></div>");
-elem.id = 'summernote';
+    // create a new div element 
+const newDiv = document.createElement("div"); 
+newDiv.id = "summernote";
+el.appendChild(newDiv);
+}
+//*[@id="dash"]/div/div/div/div/div/form/div[2]/div/dl/dd
+
 // create an observer instance
   function log(mutations) {
     for (let mutation of mutations) {
@@ -10,14 +16,27 @@ elem.id = 'summernote';
     }
 }
 let observer = new MutationObserver(log);
-onload = function() {
-    observer.observe(elem, {
-        childList: true
-    });
+// onload = function() {
+//     observer.observe(elem, {
+//         childList: true
+//     });
 
-    $('#summernote').summernote();     
-}
- 
+// }
+    $(document).ready(function() {
+    const elem = document.querySelector('textarea.form-control');
+        // create a new div element 
+    //const newDiv = document.createElement("div"); 
+   // newDiv.id = "summernote";
+   // elem.appendChild(newDiv);
+   elem.id = "summernote";
+    $("#summernote").removeClass('display', 'none');
+    $("#summernote").addClass('display', 'block');
+    $('#summernote').summernote({
+        placeholder: 'Hello Bootstrap 4',
+        tabsize: 2,
+        height: 100
+      });  
+    });
 // later, you can stop observing
 observer.disconnect();
 
